8312ed5c67e5e5856b17508f4f157805a18f1418
Reka Szenes <rekabrigitta.sz@gmail.com>
2021-10-27 11:23:59 +0200

    responsive nav bar

---
+++ app.js
@@ -0,0 +1,31 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+        // toggle NAV
+        nav.classList.toggle('nav-active');
+
+        // animate LINKS
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+            }
+
+        });
+        //burger ANIMATION
+        burger.classList.toggle('toggle');
+    });
+
+
+
+}
+
+// Call your functions here
+// const app = ()=>{
+// }
+
+navSlide();
