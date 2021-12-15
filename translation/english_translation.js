02557f15755f26f7ef31fd51bc9fdfc5f33258df
Reka Vass <rekabrigitta.sz@gmail.com>
2021-12-15 15:00:36 +0100

    adding translation to the pages

---
+++ translation/english_translation.js
@@ -0,0 +1,133 @@
+var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "German",
+            "lang_code": "de-CH",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        }
+    ],
+    "translated_segments": [
+        {
+            "source": "Vass Polsterreinigung",
+            "target": "Vass Upholstery Cleaning"
+        },
+        {
+            "source": "Preise",
+            "target": "Prices"
+        },
+        {
+            "source": "Kontakt",
+            "target": "Contact Me"
+        },
+        {
+            "source": "Lassen Sie Ihre Polster reinigen, statt neu zu kaufen!",
+            "target": "Have your upholstery cleaned instead of buying new!"
+        },
+        {
+            "source": "In Komfort von Ihrem Zuhause.",
+            "target": "In the comfort of your home."
+        },
+        {
+            "source": "Mit mobile Polsterreinigung Geräte werden Ihre Polstermöbel hygienisch sauber.",
+            "target": "With mobile upholstery cleaning devices, your upholstered furniture will be hygienically clean."
+        },
+        {
+            "source": "Bequem vor Ort, auch am Wochenende!",
+            "target": "Conveniently in your home, even on weekends!"
+        },
+        {
+            "source": "Wie setzen sich die Preise für eine Polsterreinigung zusammen?",
+            "target": "How are the prices for an upholstery cleaning calculated?"
+        },
+        {
+            "source": "Die Preise sind nach folgenden Kriterien zusamenngestellt, damit die Abrechnung für Sie auch nachvollziehbar ist:",
+            "target": "The prices are calculated according to the following criteria, so that you can understand the billing process:"
+        },
+        {
+            "source": "Zustand",
+            "target": "State"
+        },
+        {
+            "source": "Größe",
+            "target": "Size"
+        },
+        {
+            "source": "Verschmutzungsgrad Ihrer Polster",
+            "target": "Degree of soiling of your upholstery"
+        },
+        {
+            "source": "Kilometer zuschlag: ab 20 Km von Dübendorf, pro Km CHF 0.50",
+            "target": "Additonal kilometer surcharge: from 20 km from Dübendorf, CHF 0.50 per km"
+        },
+        {
+            "source": "Sie können sich die genaue Reinigungskosten gerne vorher abschätzen lassen.",
+            "target": "You can have the exact cleaning costs estimated beforehand!"
+        },
+        {
+            "source":"Stuhl*",
+            "target": "Chair*"
+        },
+        {
+            "source":"Bürostuhl",
+            "target": "Office chair*"
+        },
+        {
+            "source":"Sessel",
+            "target": "Armchair"
+        },
+        {
+            "source":"Kleinelemente (Hocker, Sitzbank)",
+            "target": "Small elements (stool, bench)"
+        },
+        {
+            "source":"Sofas 2 Sitzer",
+            "target": "Couch 2 places/seater"
+        },
+        {
+            "source":"Sofas 3 Sitzer",
+            "target": "Couch 3 places/seater"
+        },
+        {
+            "source":"Eckcouch Klein",
+            "target": "Small corner couch"
+        },
+        {
+            "source":"Eckcouch Gross",
+            "target": "Big corner couch"
+        },
+        {
+            "source":"Matratzen",
+            "target": "Mattresses"
+        },
+        {
+            "source":"* Minimum 2 Stuck",
+            "target": "* Minimum 2 pieces"
+        },
+        {
+            "source":"Alle Preise verstehen sich inklusive gesetzl. Mwst. 7.7%",
+            "target": "All prices include statutory VAT 7.7%"
+        },
+        {
+            "source":"Kontaktieren Sie mich gern per Email:",
+            "target": "Please contact me by email to arrange an appointment:"
+        },
+        {
+            "source":"Ihre E-mail Adresse:",
+            "target": "Your email address:"
+        },
+        {
+            "source":"Nachricht:",
+            "target": "Your message:"
+        },
+        {
+            "source":"Abschicken",
+            "target": "Send"
+        },
+        
+    ]
+}
